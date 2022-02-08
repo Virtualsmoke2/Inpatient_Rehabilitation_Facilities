3ed67207a3fa9a717582c142fa95a605417d5168
Virtualsmoke2 <84709354+Virtualsmoke2@users.noreply.github.com>
2022-02-08 11:53:28 +0100

    Add files via upload

---
+++ script.R
@@ -0,0 +1,61 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2580
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 ArialMT;\f1\froman\fcharset0 Times-Roman;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs32 \cf0 \expnd0\expndtw0\kerning0
+# this is my first R script 
+\f1 \
+
+\f0 # do some things
+\f1 \
+
+\f0 x = 34
+\f1 \
+
+\f0 y = 16 
+\f1 \
+
+\f0 z = x + y\'a0\'a0 # addition 
+\f1 \
+
+\f0 w = y/x\'a0\'a0\'a0\'a0 # division 
+\f1 \
+
+\f0 # display the results
+\f1 \
+
+\f0 x
+\f1 \
+
+\f0 y
+\f1 \
+
+\f0 z
+\f1 \
+
+\f0 w
+\f1 \
+
+\f0 # change x 
+\f1 \
+
+\f0 x = "some text"
+\f1 \
+
+\f0 # display the results 
+\f1 \
+
+\f0 x
+\f1 \
+
+\f0 y
+\f1 \
+
+\f0 z
+\f1 \
+
+\f0 w}
